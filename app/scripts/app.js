e8cf746c8ac8c6d4e88d02772b3bb4164a08a989
Steve Sinnwell <steve@kopfwelt.com>
2016-03-08 12:15:41 +0100

    [add] some first product frontend

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,12 +1,13 @@
 /* esversion: 6 */
+import 'style-loader!css-loader!!sass-loader!../styles/app.scss';
+import '../../node_modules/bootstrap/dist/js/bootstrap';
+// import Cart from '../components/cart/src/cart';
+// import cookie from '../components/cart/src/storage/cookie';
 
-import cart from '../components/cart/src/cart';
-import cookie from '../components/cart/src/storage/cookie';
+// const options = {
+// 	identifier: 'id'
+// };
 
-const options = {
-	identifier: 'id'
-};
-
-cart
-	.use(cookie)
-	.init(options);
+// cart
+	// .use(cookie)
+	// .init(options);
